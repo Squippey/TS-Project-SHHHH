4298ceb834c789f63afe6198141520433e314b58
Squippey <tuguiu.a01@gmail.com>
2026-09-14 01:30:09 +0300

    Grammar nazi in actiune

--- src/main.js
+++ src/main.js
@@ -9,16 +9,16 @@ document.addEventListener("DOMContentLoaded", () => {
     title: "Un an alături de tine ♥",
     startDate: "2025-09-14",
     events: [
-      { date: "14 Septembrie 2025", title: "O zi foarte speciala, pe care nu o uita niciodata", description: "Esti foarte draguta cand adormi ca pe scaunul pasagerului, inca tin minte cand ai dat un quick nap in drum spre spot." },
-      { date: "14 Septembrie 2026", title: " 'Insert unoffical 'move in with me' invitation'", description: "M-i se pare foarte frumos cum se nimereste sa incepem sa locuim impreuna in acelasi timp in care facem un an de relatie" }
+      { date: "14 Septembrie 2025", title: "O zi foarte speciala, pe care nu o voi uita niciodata", description: "Esti foarte draguta cand adormi pe scaunul pasagerului, inca tin minte cand ai dat un quick nap in drum spre spot." },
+      { date: "14 Septembrie 2026", title: " 'Insert unoffical 'move in with me' invitation'", description: "Mi se pare foarte frumos cum se nimereste sa incepem sa locuim impreuna in acelasi timp in care facem un an de relatie" }
     ],
     letterTitle: "",
     paragraphs: [
       "Hello Stinky,",
       "Nu am idee daca asta poate compensa pentru ce ai confectionat tu pentru mine, dar am vrut sa-ti scriu și eu cateva randuri.",
-      "Vreau să știi cat de recunoscator sunt pentru ca esti iubita mea, faptul ca incerci incerci sa cresti cu fiecare zi, faptul incerci sa treci peste greutatile tale din trecut ca sa ma iubesti cat poti tu de mult si faptul ca ma sustii cat stii tu de bine.",
-      "Spre ca de acum incolo sa avem din ce in mai multi ani impreuna, pana cand nu vom mai putea tine cont de cat timp suntem impreuna, si sa mergem sa vizitam multe locatii impreuna ca sa completam albumul ala de care mentionam ca vrem sa-l facem, si in mare, sa cladim impreuna un viitor frumos, doar pentru noi",
-      "Abia astept, de asemenea, sa te apuci si tu de carnet, sa-l iei, si sa ne plimbam cu masinile impreuna, atunci jocurile pe PC pur si simplu sunt prea plictisitoare.",
+      "Vreau să știi cat de recunoscator sunt pentru ca esti iubita mea, faptul ca incerci sa cresti cu fiecare zi, faptul ca incerci sa treci peste greutatile tale din trecut ca sa ma iubesti cat poti tu de mult si faptul ca ma sustii cat stii tu de bine.",
+      "Spre ca de acum incolo sa avem din ce in ce mai multi ani impreuna, pana cand nu vom mai putea tine cont de cat timp suntem impreuna, si sa mergem sa vizitam multe locatii impreuna ca sa completam albumul ala de care mentionam ca vrem sa-l facem, si in mare, sa cladim impreuna un viitor frumos, doar pentru noi",
+      "Abia astept, de asemenea, sa te apuci si tu de carnet, sa-l iei, si sa ne plimbam cu masinile impreuna, atunci cand jocurile pe PC pur si simplu sunt prea plictisitoare.",
       "Also cum scrie si mai sus, si cum era deja evident, asta poti zice ca e o invitatie neoficiala, oficiala de a te muta cu mine.",
     ],
     signature: "Luv u 3000 <3",
